d08dc7aff0113e5684123fbc3723eeab2acca50f
zhangzhuofei1 <z15903509849@163.com>
2025-03-24 19:45:00 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/Front-end-effects/'
-    : '/',
+    publicPath: /Front-end-effects/,
     configureWebpack: {
         module: {
             rules: [{
